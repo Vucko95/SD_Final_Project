f02550d10d35c6710512afcea19e90b48aeb20a2
Ronald Suplina <tyger995@gmail.com>
2022-11-08 22:13:49 +0100

    Home Defined

---
+++ dog-surf-react/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+import Home from './components/pages/Home';
+
+import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
+
+function App(){
+    return (
+    <>
+        <Router >
+    
+                <Switch>
+                    <Route path='/' exact component={Home}/>
+                </Switch>
+        </Router>
+       
+    </>
+    );
+}
+
+export default App;
---
+++ dog-surf-react/src/components/pages/Home.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import '../../App.css';
+
+
+function Home() {
+  return (
+    <>
+Hello
+    </>
+  );
+}
+
+export default Home;
