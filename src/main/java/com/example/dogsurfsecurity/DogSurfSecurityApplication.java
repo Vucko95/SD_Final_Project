e06687a0cdffc20b14f41df12b9586f3d809fda7
Ronald Suplina <tyger995@gmail.com>
2022-11-25 22:53:17 +0100

    Pom+Main

---
+++ src/main/java/com/example/dogsurfsecurity/DogSurfSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.dogsurfsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogSurfSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogSurfSecurityApplication.class, args);
+	}
+
+}
