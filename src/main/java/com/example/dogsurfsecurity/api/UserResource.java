0749c8557ebf2667a5c70b99fb48131af592042e
Ronald Suplina <tyger995@gmail.com>
2022-11-25 22:52:29 +0100

    Basic Endpoints

--- src/main/java/com/example/dogsurfsecurity/api/UserResource.java
+++ src/main/java/com/example/dogsurfsecurity/api/UserResource.java
@@ -1,13 +1,14 @@
 package com.example.dogsurfsecurity.api;
 
+import com.example.dogsurfsecurity.domain.Role;
 import com.example.dogsurfsecurity.domain.User;
 import com.example.dogsurfsecurity.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,5 +22,16 @@ public class UserResource {
     @GetMapping("/users")
     public ResponseEntity<List<User>>getUsers(){
         return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<User>saveUser(@RequestBody User user){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+    @PostMapping("/role/save")
+    public ResponseEntity<Role>saveRole(@RequestBody Role role){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
     }
 }
