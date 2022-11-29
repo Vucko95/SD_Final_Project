9d972039df848865b6c93d80229190181f1717d0
Ronald Suplina <tyger995@gmail.com>
2022-11-29 11:43:19 +0000

    Backend From other Branch

---
+++ backend/dogsurf/src/main/java/dogsurf/DogSurfWebApplication.java
@@ -0,0 +1,13 @@
+package dogsurf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogSurfWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogSurfWebApplication.class, args);
+	}
+
+}
---
+++ backend/dogsurf/src/test/java/dogsurf/DogSurfWebApplicationTests.java
@@ -0,0 +1,13 @@
+package dogsurf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogSurfWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
